fdc67ae7a1a35b01144aa1ad08d45d5540aa7e85
Guillermo San Martín <guille.seba0802@gmail.com>
2023-06-21 23:30:59 -0300

    change button props

--- src/components/ActionButton.jsx
+++ src/components/ActionButton.jsx
@@ -8,7 +8,7 @@ const ActionButton = ({
   return (
     <button
       disabled={disabled}
-      className={`${width} ${mdw} ${position} dark:bg-blue-600 bg-green-500 text-sm px-3 py-2 rounded-md text-white font-bold dark:hover:bg-blue-500 hover:bg-green-400 active:translate-y-1 disabled:bg-green-200 dark:disabled:bg-blue-200`}
+      className={`${width} ${mdw} ${position} dark:bg-blue-600 bg-green-500 text-sm px-3 py-2 rounded-md text-white font-bold dark:hover:bg-blue-500 hover:bg-green-400 active:translate-y-1 active:disabled:translate-y-0 disabled:bg-green-200 dark:disabled:bg-blue-200`}
     >
       {text}
     </button>
