7607f9a03b9a3cf3e034ee89a9bb2a4b339cf3d9
Guillermo San Martín <guille.seba0802@gmail.com>
2023-06-21 23:38:42 -0300

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import ArticleDetail from './components/ArticleDetail';
-import Footer from './components/Footer';
-import Login from './components/Login';
-import Main from './components/Main';
-import Navbar from './components/Navbar';
-import SearchFeed from './components/SearchFeed';
-import Signup from './components/Signup';
 import ContextContainer from './components/UserContext';
-import UserProfile from './components/UserProfile';
 import ThemeContextContainer from './components/ThemeContext';
+import { Suspense, lazy } from 'react';
+import Loading from './components/Loading';
+
+const ArticleDetail = lazy(() => import('./components/ArticleDetail'));
+const Footer = lazy(() => import('./components/Footer'));
+const Login = lazy(() => import('./components/Login'));
+const Main = lazy(() => import('./components/Main'));
+const Navbar = lazy(() => import('./components/Navbar'));
+const SearchFeed = lazy(() => import('./components/SearchFeed'));
+const Signup = lazy(() => import('./components/Signup'));
+const UserProfile = lazy(() => import('./components/UserProfile'));
 
 function App() {
   return (
-    <ThemeContextContainer>
-      <ContextContainer>
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route path='/:page?' element={<Main />} />
-            <Route path='/article/:id' element={<ArticleDetail />} />
-            <Route path='/signup' element={<Signup />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/user/:id/:page?' element={<UserProfile />} />
-            <Route path='/search/:searchString?' element={<SearchFeed />} />
-          </Routes>
-          <Footer />
-        </BrowserRouter>
-      </ContextContainer>
-    </ThemeContextContainer>
+    <Suspense fallback={<Loading />}>
+      <ThemeContextContainer>
+        <ContextContainer>
+          <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route path='/:page?' element={<Main />} />
+              <Route path='/article/:id' element={<ArticleDetail />} />
+              <Route path='/signup' element={<Signup />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/user/:id/:page?' element={<UserProfile />} />
+              <Route path='/search/:searchString?' element={<SearchFeed />} />
+            </Routes>
+            <Footer />
+          </BrowserRouter>
+        </ContextContainer>
+      </ThemeContextContainer>
+    </Suspense>
   );
 }
 
